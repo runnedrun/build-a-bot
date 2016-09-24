484db64c0d385c1ad65072db8eec820001975e5d
David Gaynor <runnedrun@gmail.com>
2016-09-23 17:25:16 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var gameInput = $("#bot-input");
 var gameOutput  = $("#bot-output");
-gameInput.keydown(function(keydownEvent) {
+gameInput.keydown(function(keydownEvent) {  
   // the key code for enter is 13
   if (keydownEvent.keyCode == 13) {      
     parseText(gameInput.val()); 
